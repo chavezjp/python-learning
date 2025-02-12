4e42e7f643f7b9dcd3a579335913622e8d2730ec
chavezjp <38542395+chavezjp@users.noreply.github.com>
2025-02-12 11:27:48 -0600

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,20 @@
+print("Hello, World!")
+name = "JP"
+print("My name is", name)
+
+# Mini Exercises
+# Print "Welcome to Python Programming!"
+print("Welcome to Python Programming!")
+
+# Create a variable fruit = "apple" and print fruit
+
+fruit = "apple"
+print(fruit)
+
+# Ask for user input: What is your favorite color? and print it
+
+fav_col = input("What is your favorite color?")
+print("Your favorite color is",fav_col)
+
+
+
